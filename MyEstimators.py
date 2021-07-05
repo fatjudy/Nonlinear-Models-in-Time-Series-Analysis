c80d05b12c4a7ed12b624dfc5ed7a357f00a50f0
Ying Zhou <fatjodie0510@yahoo.com>
2021-07-05 22:43:52 +1000

    Index method is changed. Full-sample results obtained in loop.

--- MyEstimators.py
+++ MyEstimators.py
@@ -27,7 +27,7 @@ def single_index(x):
 
 class CLS_Estimator(BaseEstimator, RegressorMixin):
 
-    def __init__(self, obj_func=None, x0=0, method='SLSQP', constraints=(), options={'maxiter':10000}):
+    def __init__(self, obj_func=None, x0=0, method='SLSQP', constraints=(), options={'maxiter':1000}):
         self.obj_func = obj_func
         self.x0 = x0
         self.method = method
@@ -42,8 +42,7 @@ class CLS_Estimator(BaseEstimator, RegressorMixin):
     #             con += params[j] ** 2
     #             cons = con - 1
     #         return cons
-
-        # return {'type': 'eq', 'fun': constraint}
+    #     return {'type': 'eq', 'fun': constraint}
 
     def loss(self, x, y):
         def loss_func(params):
